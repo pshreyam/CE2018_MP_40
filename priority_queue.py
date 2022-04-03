bf8ce630c9015df9cc9adc62f328a007c17d7ed6
Shreyam Pokharel <pshreyam@gmail.com>
2022-04-03 22:30:22 +0545

    Add skeleton code

---
+++ priority_queue.py
@@ -0,0 +1,38 @@
+class MaxPriorityQueue():
+    """Implementation of maximum priority queue using binary heap."""
+
+    def __init__(self):
+        """Initialize an instance of maximum priority queue."""
+        pass
+
+    def insert(x):
+        """Insert new element in the priority queue.
+
+        Args:
+            x
+                The element to be inserted.
+        """
+        pass
+
+    def maximum():
+        """Return the element with the maximum priority (or the largest key)."""
+        pass
+
+    def extract_max():
+        """Remove and return the element with the maximum priority (or the largest key)."""
+        pass
+
+    def increase_key(x, k):
+        """Increase the value of element x's key to new value k.
+
+        Args:
+            x
+                The element whose priority has to be increased.
+            k
+                The amount to be set as the new key of x.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    mpq = MaxPriorityQueue()
---
+++ test_priority_queue.py
@@ -0,0 +1,7 @@
+import unittest
+
+from priority_queue import MaxPriorityQueue
+
+
+class TestMaxPriorityQueue(unittest.TestCase):
+    pass
