509527a2f1edb0e50dc72c7f5222cd53e2aa58ad
Shreyam Pokharel <pshreyam@gmail.com>
2022-04-04 22:34:44 +0545

    Add basic structure of the code with reference to the book 'Introduction to Algorithms'

--- priority_queue.py
+++ priority_queue.py
@@ -1,37 +1,86 @@
+import sys
+
+
 class MaxPriorityQueue():
     """Implementation of maximum priority queue using binary heap."""
 
     def __init__(self):
         """Initialize an instance of maximum priority queue."""
-        pass
+        self.heap = list()
+        self.size = 0
 
-    def insert(x):
+    def __parent(index):
+        """Return the index of the parent."""
+        return (index - 1) // 2
+
+    def __left_child(index):
+        """Return the index of the left child."""
+        return 2 * index + 1
+
+    def __right_child(index):
+        """Return the index of the right child."""
+        return 2 * index + 2
+
+    def __max_heapify(self, index):
+        """Maintain the max-heap property by swapping with the largest child in
+        case the max-heap property is violated."""
+        l = self.__left_child(index)
+        r = self.__right_child(index)
+        if l <= self.size and self.heap[l] > self.heap[index]:
+            largest = l
+        else:
+            largest = index
+        if r <= self.size and self.heap[r] > self.heap[largest]:
+            largest = r
+        if largest != index:
+            # Exchange the node with its largest child and recursively move down to maintain max-heap property
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self.__max_heapify(largest)
+
+    def insert(key):
         """Insert new element in the priority queue.
 
         Args:
-            x
-                The element to be inserted.
+            key
+                The priority of the element to be inserted.
         """
-        pass
+        self.size += 1
+        # Set the last element's priority to a very small number initially and then finally increasing it to its actual value
+        self.heap[self.size-1] = -sys.maxsize
+        self.increase_key(self.size-1, key)
 
     def maximum():
         """Return the element with the maximum priority (or the largest key)."""
-        pass
+        if self.size < 1:
+            print("Heap underflow!")
+            return -1
+        return self.heap[0]
 
     def extract_max():
         """Remove and return the element with the maximum priority (or the largest key)."""
-        pass
+        if self.size < 1:
+            print("Heap underflow!")
+            return -1
+        max_element = self.heap[0]
+        self.heap[0] = self.heap[self.size-1]
+        self.__max_heapify(0)
 
-    def increase_key(x, k):
-        """Increase the value of element x's key to new value k.
+    def increase_key(index, key):
+        """Increase the value of element in position 'index' to new value key.
 
         Args:
-            x
-                The element whose priority has to be increased.
-            k
-                The amount to be set as the new key of x.
+            index
+                The index of the element whose priority has to be increased.
+            key
+                The new priority for the element in position 'index'.
         """
-        pass
+        if key < self.heap[index]:
+            print("New key is smaller than the current key!")
+        self.heap[index] = key
+        while i > 0 and self.heap[self.__parent(i)] < self.heap[i]:
+            # Exchange the element with its parent
+            self.heap[i], self.heap[self.__parent(i)] = self.heap[self.__parent(i)], self.heap[i]
+            i = self.__parent(i)
 
 
 if __name__ == "__main__":
