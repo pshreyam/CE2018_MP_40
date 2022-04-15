2fe9347cb0be78065336803a51554c95860992ce
Shreyam Pokharel <pshreyam@gmail.com>
2022-04-15 21:02:29 +0545

    Add test cases for 'extract_max' and 'increase_key' methods for MaxPriorityQueue class
    Also fix error in extract_max method

--- priority_queue.py
+++ priority_queue.py
@@ -3,6 +3,12 @@ import sys
 
 class Process:
     def __init__(self, key, value):
+        """Initialize a process with priority 'key' and value 'value'.
+
+        It represents the jobs/processes in a shared computer with certain
+        priority associated with it. The value represents certain attribute
+        associated with the job/process.
+        """
         self.key = key
         self.value = value
 
@@ -12,7 +18,7 @@ class MaxPriorityQueue():
 
     def __init__(self):
         """Initialize an instance of maximum priority queue."""
-        self.heap = list()
+        self.__heap = list()
         self.__size = 0
 
     def __parent(self, index):
@@ -32,20 +38,23 @@ class MaxPriorityQueue():
         case the max-heap property is violated."""
         l = self.__left_child(index)
         r = self.__right_child(index)
-        if l <= self.__size and self.heap[l].key > self.heap[index].key:
+        if l <= self.__size and self.__heap[l].key > self.__heap[index].key:
             largest = l
         else:
             largest = index
-        if r <= self.__size and self.heap[r].key > self.heap[largest].key:
+        if r <= self.__size and self.__heap[r].key > self.__heap[largest].key:
             largest = r
         if largest != index:
             # Exchange the node with its largest child and recursively move down to maintain max-heap property
-            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self.__heap[index], self.__heap[largest] = self.__heap[largest], self.__heap[index]
             self.__max_heapify(largest)
 
     @property
     def priority_queue(self):
-        return list(map(lambda x: x.key, self.heap))
+        """Return the heap (list) associated with the priority queue."""
+        return list(map(lambda x: x.key, self.__heap))
+
+    # Max Priority Queue Methods
 
     def insert(self, key, value):
         """Insert new element in the priority queue.
@@ -59,23 +68,24 @@ class MaxPriorityQueue():
         self.__size += 1
         # Set the last element's priority to a very small number initially
         process = Process(-sys.maxsize, value)
-        self.heap.append(process)
+        self.__heap.append(process)
         self.increase_key(self.__size-1, key)
 
     def maximum(self):
         """Return the element with the maximum priority (or the largest key)."""
         if self.__size < 1:
-            print("Heap underflow!")
-            return -1
-        return self.heap[0].key
+            # ERROR: Heap underflow!
+            return False
+        return self.__heap[0].key
 
     def extract_max(self):
         """Remove and return the element with the maximum priority (or the largest key)."""
         if self.__size < 1:
-            print("Heap underflow!")
-            return -1
-        max_element = self.heap[0]
-        self.heap[0] = self.heap[self.__size-1]
+            # ERROR: Heap underflow!
+            return False
+        max_element = self.__heap[0]
+        self.__heap[0] = self.__heap[self.__size-1]
+        self.__size -= 1
         self.__max_heapify(0)
         return max_element.key
 
@@ -88,21 +98,11 @@ class MaxPriorityQueue():
             key
                 The new priority for the element in position 'index'.
         """
-        if key < self.heap[index].key:
-            print("New key is smaller than the current key!")
-        self.heap[index].key = key
-        while index > 0 and self.heap[self.__parent(index)].key < self.heap[index].key:
+        if key < self.__heap[index].key:
+            # ERROR: New key is smaller than the current key!
+            return False
+        self.__heap[index].key = key
+        while index > 0 and self.__heap[self.__parent(index)].key < self.__heap[index].key:
             # Exchange the element with its parent
-            self.heap[index], self.heap[self.__parent(index)] = self.heap[self.__parent(index)], self.heap[index]
+            self.__heap[index], self.__heap[self.__parent(index)] = self.__heap[self.__parent(index)], self.__heap[index]
             index = self.__parent(index)
-
-
-if __name__ == "__main__":
-    mpq = MaxPriorityQueue()
-
-    mpq.insert(10, "Value for 10")
-    mpq.insert(2, "Value for 2")
-    mpq.insert(20, "Value for 20")
-    # mpq.insert(30)
-    # mpq.insert(15)
-    print(mpq.priority_queue)
--- test_priority_queue.py
+++ test_priority_queue.py
@@ -6,33 +6,59 @@ from priority_queue import MaxPriorityQueue
 class TestMaxPriorityQueue(unittest.TestCase):
     def setUp(self):
         self.mpq = MaxPriorityQueue()
+        self.mpq.insert(10, "Value for 10")
+        self.mpq.insert(2, "Value for 2")
+        self.mpq.insert(20, "Value for 20")
+        self.mpq.insert(30, "Value for 30")
+        self.mpq.insert(15, "Value for 15")
 
     def test_insert(self):
-        self.mpq.insert(10, "Value for 10")
-        self.assertListEqual(self.mpq.priority_queue, [10])
-        self.mpq.insert(2, "Value for 2")
-        self.assertListEqual(self.mpq.priority_queue, [10, 2])
-        self.mpq.insert(20, "Value for 20")
-        self.assertListEqual(self.mpq.priority_queue, [20, 2, 10])
-        self.mpq.insert(30, "Value for 30")
-        self.assertListEqual(self.mpq.priority_queue, [30, 20, 10, 2])
-        self.mpq.insert(15, "Value for 15")
-        self.assertListEqual(self.mpq.priority_queue, [30, 20, 10, 2, 15])
-
-    def test_maximum(self):
-        self.mpq.insert(10, "Value for 10")
-        self.assertEqual(self.mpq.maximum(), 10)
-        self.mpq.insert(2, "Value for 2")
-        self.assertEqual(self.mpq.maximum(), 10)
-        self.mpq.insert(20, "Value for 20")
-        self.assertEqual(self.mpq.maximum(), 20)
-        self.mpq.insert(30, "Value for 30")
-        self.assertEqual(self.mpq.maximum(), 30)
-        self.mpq.insert(15, "Value for 15")
-        self.assertEqual(self.mpq.maximum(), 30)
-
-    def test_extract_max(self):
-        pass
+        mpq_for_insertion = MaxPriorityQueue()
+        mpq_for_insertion.insert(10, "Value for 10")
+        self.assertListEqual(mpq_for_insertion.priority_queue, [10])
+        mpq_for_insertion.insert(2, "Value for 2")
+        self.assertListEqual(mpq_for_insertion.priority_queue, [10, 2])
+        mpq_for_insertion.insert(20, "Value for 20")
+        self.assertListEqual(mpq_for_insertion.priority_queue, [20, 2, 10])
+        mpq_for_insertion.insert(30, "Value for 30")
+        self.assertListEqual(mpq_for_insertion.priority_queue, [30, 20, 10, 2])
+        mpq_for_insertion.insert(15, "Value for 15")
+        self.assertListEqual(mpq_for_insertion.priority_queue, [30, 20, 10, 2, 15])
 
     def test_increase_key(self):
-        pass
+        # Initially: [30, 20, 10, 2, 15]
+        self.mpq.increase_key(4, 40)
+        # Before heapify: [30, 20, 10, 2, 40]
+        # Expected (After heapify): [40, 30, 10, 2, 20]
+        self.assertListEqual(self.mpq.priority_queue, [40, 30, 10, 2, 20])
+        self.mpq.increase_key(2, 50)
+        # Before heapify: [40, 30, 50, 2, 20]
+        # Expected (After heapify): [50, 30, 40, 2, 20]
+        self.assertListEqual(self.mpq.priority_queue, [50, 30, 40, 2, 20])
+        # Key in index 2 (30) is greater than key provided (10)
+        self.assertEqual(self.mpq.increase_key(2, 10), False)
+
+    def test_maximum(self):
+        self.assertEqual(self.mpq.maximum(), 30)
+        # Less than the smallest key in the priority queue
+        self.mpq.insert(1, "Value for 1")
+        self.assertEqual(self.mpq.maximum(), 30)
+        # Intermediate key between the min and max key
+        self.mpq.insert(5, "Value for 5")
+        self.assertEqual(self.mpq.maximum(), 30)
+        # Greater than the largest key in the priority queue
+        self.mpq.insert(70, "Value for 70")
+        self.assertEqual(self.mpq.maximum(), 70)
+
+        # New empty priority queue
+        mpq_for_maximum = MaxPriorityQueue()
+        self.assertEqual(mpq_for_maximum.maximum(), False)
+
+    def test_extract_max(self):
+        self.assertEqual(self.mpq.extract_max(), 30)
+        self.assertEqual(self.mpq.extract_max(), 20)
+        self.assertEqual(self.mpq.extract_max(), 15)
+        self.assertEqual(self.mpq.extract_max(), 10)
+        self.assertEqual(self.mpq.extract_max(), 2)
+        # Heap underflow
+        self.assertEqual(self.mpq.extract_max(), False)
